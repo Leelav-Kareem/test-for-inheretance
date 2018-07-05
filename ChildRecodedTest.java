ac47b70c6221ab7f1a296ef33e0c7f82aa12fe78
AsiaSaeed <40139873+AsiaSaeed@users.noreply.github.com>
2018-07-05 15:47:15 +0300

    Add files via upload

---
+++ ChildRecodedTest.java
@@ -0,0 +1,9 @@
+public class ChildRecodedTest extends ParentRecodedTest
+{
+	public int priceMultiplieNumber(int y ,int x)
+	{
+		int price =y*2;
+		int number=x+1;
+		return(price*number);
+	}
+}
---
+++ ParentRecodedtest.java
@@ -0,0 +1,33 @@
+class ParentRecodedTest
+{
+	private int style;
+	private String color;
+	public int getStyle()
+	{
+		return style;
+	}
+	public void setStyle(int styleHatw)
+	{
+		style=styleHatw;
+	}
+	public String getColor()
+	{
+		return color;
+	}
+	public void  setColor(String colorHatw)
+	{
+		color=colorHatw;
+	}
+	public void setColor()
+	{
+		color=null;
+	}
+	public final String clothesCompanyName="tita";
+	public int priceMultiplieNumbers(int priceHatw,int numberHatw)
+	{
+		int price=priceHatw;
+		int number =numberHatw;
+		return (price*number);
+	}
+
+}
